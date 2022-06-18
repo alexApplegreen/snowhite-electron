a7aa116ec7f0f8b9d2ec227cb050c575aadf7b1e
Alexander Tepe <a.tepe@kalverkamp.de>
2022-06-19 00:13:40 +0200

    implemented currentweather

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,25 @@
 			<h3 style="margin-top: 10px; margin-left: 10px;">{{ date_str }}</h3>
 			<h1 style="margin-top: 10px; margin-left: 10px;">{{ time_str }}</h1>
 		</div>
+		<h1 class="weatherHeader" style="color: aliceblue">Wetter in {{ location }}</h1>
 		<div class="weather">
 			<img
 				class="weatherIcon"
-				v-if="is_sunny" 
+				v-if="is_sunny"
 				src="./assets/sun.png"
 			/>
+			<img
+				class="weatherIcon"
+				v-if="is_cloudy"
+				src="./assets/cloud.png"
+			/>
+			<div class="weatherData" style="display: inline-block; margin-left: 10px">
+				<h1 style="position: absolute; top: 0">{{ description }}</h1>
+				<h1 style="position: absolute; top: 35px">{{ temperature }}</h1>
+				<h3 style="position: absolute; top: 80px">Max: {{ temp_max }}</h3>
+				<h3 style="position: absolute; top: 100px">Min: {{ temp_min }}</h3>
+			</div>
+			
 		</div>
 		<div class="mails">
 			<h1 class="widgetHeader">Mails</h1>
@@ -29,6 +42,8 @@ export default {
 		this.testConnection();
 		this.getTime();
 		this.getWeather();
+
+		this.startUpdates();
 	},
 	data() {
 		return {
@@ -39,9 +54,14 @@ export default {
 			date_str: "",
 			time_str: "",
 
-			weather_req_url: "https://api.openweathermap.org/data/2.5/weather?lat=52.279911&lon=8.047179&units=metric&appid=375424fb53107d54d2fb666df917ff61&lang=de",
-			is_sunny: true,
-			is_cloudy: false
+			weather_req_url: "https://api.openweathermap.org/data/2.5/weather?lat=52.279911&lon=8.047179&units=metric&lang=de&appid=375424fb53107d54d2fb666df917ff61",
+			is_sunny: false,
+			is_cloudy: false,
+			description: "",
+			temperature: "",
+			temp_min: "",
+			temp_max: "",
+			location: ""
 		}
 	},
 	methods: {
@@ -50,23 +70,44 @@ export default {
 		},
 
 		getTime() {	
-			setInterval(() => {
-				this.datetime = new Date();
-				this.time_str = this.datetime.toLocaleTimeString();
-				this.date_str = this.datetime.toLocaleDateString();
-			}, 1000)
+			this.datetime = new Date();
+			this.time_str = this.datetime.toLocaleTimeString();
+			this.date_str = this.datetime.toLocaleDateString();
 		},
 
 		getWeather() {
+			axios.get(this.weather_req_url).then((res) => {
+				this.description = res.data["weather"][0]["description"];
+				let main = res.data["weather"][0]["main"];
+				console.log(main);
+				if (main === "Clouds") {
+					this.is_cloudy = true;
+					this.is_sunny = false;
+				}
+				else if (main === "Sun") {
+					this.is_cloudy = false;
+					this.is_sunny = true;
+				}
+				this.temperature = res.data["main"]["temp"] + " °C";
+				this.temp_max = res.data["main"]["temp_max"] + " °C";
+				this.temp_min = res.data["main"]["temp_min"] + " °C";
+				this.location = res.data["name"];
+				console.log(this.location);
+			}).catch((err) => {
+				this.error_present = true;
+				console.warn(err);
+				this.error_message = "Could not retrieve Weather information";
+			});
+		},
+
+		startUpdates() {
 			setInterval(() => {
-					axios.get(this.weather_req_url).then((res) => {
-					console.log(res);
-					// TODO parse and display
-				}).catch(() => {
-					this.error_present = true;
-					this.error_message = "Could not retrieve Weather information";
-				});
-			}, 1000 * 60)
+				this.getTime();
+			}, 1000);
+
+			setInterval(() => {
+				this.getWeather();
+			}, 1000 * 60);
 		}
 	}
 }
@@ -102,15 +143,21 @@ export default {
 	position: absolute;
 	height: 150px;
 	width: 99%;
-	border: 1px solid white;
-	border-radius: 4px;
 	bottom: 0;
 }
 
 .weatherIcon {
 	height: 130px;
 	margin-left: 10px;
-	margin-top:10px
+	margin-top: 10px;
+	position: absolut;
+	left: 0;
+}
+
+.weatherHeader {
+	position: absolute;
+	margin-left: 10px;
+	bottom: 130px;
 }
 
 .mails {
